6d6f9fd90d246cf979a637c0ab938a5869c090c6
Vera van den Heuvel <ray10k@xmsnet.nl>
2024-12-02 09:49:11 +0100

    day 2 star 2

--- days/day_02.py
+++ days/day_02.py
@@ -14,25 +14,47 @@ def parse_input(input_content:str) -> list[IType]:
         retval.append(ReactorReport(levels))
     return retval
 
+def check_falling(l,r) -> bool:
+    return l > r and (l - r) in range(1,4)
+
+def check_rising(l,r) -> bool:
+    return r > l and (r - l) in range(1,4)
+
+
+def skip_it(skip:int,to_iterate:list,start:int = 0):
+    for index, item in enumerate(to_iterate,start=start):
+        if index != skip:
+            yield item
+
 def star_one(data:list[IType]) -> str:
     safe_count = 0
     
-    def check_falling(l,r) -> bool:
-        return l > r and (l - r) in range(1,4)
-
-    def check_rising(l,r) -> bool:
-        return r > l and (r - l) in range(1,4)
-    
     for report in data:
         lvl = report.levels
-        check = check_rising if lvl[0] < lvl[1] else check_falling
+        check = check_rising if lvl[0] < lvl[-1] else check_falling
         if all(check(l,r) for l,r in zip(lvl,lvl[1:])):
             safe_count += 1
     
     return f"{safe_count}"
 
 def star_two(data:list[IType]) -> str:
-    pass
+    safe_count = 0
+    
+    for report in data:
+        lvl = report.levels
+        if all(check_rising(l,r) for l,r in zip(lvl,lvl[1:])) or all(check_falling(l,r) for l,r in zip(lvl,lvl[1:])):
+            safe_count += 1
+            continue
+        for skip in range(len(lvl)):
+            skip_list = list(skip_it(skip,lvl))
+            if all(check_rising(l,r) for l,r in zip(skip_list,skip_list[1:])):
+                safe_count += 1
+                break
+            if all(check_falling(l,r) for l,r in zip(skip_list,skip_list[1:])):
+                safe_count += 1
+                break
+            
+    return f"{safe_count}"
 
 if __name__ == "__main__":
     from pathlib import Path
