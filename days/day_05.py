8fbaafdb42cb7ca3fc18ebf9cd1f1bec79dbfea2
Vera van den Heuvel <ray10k@xmsnet.nl>
2024-12-05 08:47:53 +0100

    day 5 star 2

--- days/day_05.py
+++ days/day_05.py
@@ -6,6 +6,14 @@ class OrderRule:
     entire sequence to be valid."""
     left:int
     right:int
+    
+    def rule_applies(self,values:list[int]) -> bool:
+        return self.left in values and self.right in values
+    
+    def check_rule(self,values:list[int]) -> bool:
+        if self.left not in values or self.right not in values:
+            return True
+        return values.index(self.left) < values.index(self.right)
 
 IType = tuple[list[OrderRule],list[list[int]]]
 
@@ -30,19 +38,37 @@ def star_one(data:IType) -> str:
     retval = 0
     for seq in sequences:
         for rule in rules:
-            try:
-                l_index, r_index = seq.index(rule.left),seq.index(rule.right)
-                if l_index > r_index:
-                    break
-            except:
-                continue
+            if not rule.check_rule(seq):
+                break
         else:
             midpoint = int((len(seq) - 1) / 2)
             retval += seq[midpoint]
     return f"{retval}"
 
 def star_two(data:IType) -> str:
-    pass
+    rules, sequences = data
+    retval = 0
+    unsorted = list()
+    for seq in sequences:
+        for rule in rules:
+            if not rule.check_rule(seq):
+                unsorted.append(seq)
+                break
+    
+    for to_sort in unsorted:
+        sort_rules = [rule for rule in filter(lambda x: x.rule_applies(to_sort),rules)]
+        reorder = [x for x in to_sort]
+        while not all(r.check_rule(reorder) for r in sort_rules):
+            for r in sort_rules:
+                if not r.check_rule(reorder):
+                    l_index = reorder.index(r.left)
+                    r_index = reorder.index(r.right)
+                    temp = reorder[l_index]
+                    reorder[l_index] = reorder[r_index]
+                    reorder[r_index] = temp
+        midpoint = int((len(reorder) - 1) / 2)
+        retval += reorder[midpoint]
+    return f"{retval}"
 
 if __name__ == "__main__":
     from pathlib import Path
