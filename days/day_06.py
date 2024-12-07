8587d4491f7f6d2c4aa9730b2a8c1f042bee873c
Vera van den Heuvel <ray10k@xmsnet.nl>
2024-12-07 16:37:49 +0100

    day 6 star 2 (incomplete)

--- days/day_06.py
+++ days/day_06.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable
 from dataclasses import dataclass
 from itertools import cycle
 
@@ -6,9 +7,15 @@ class Coordinate:
     """A single location in the map."""
     x:int
     y:int
+    
+    def step(self,direction:tuple[int,int]) -> "Coordinate":
+        return Coordinate(self.x + direction[0], self.y + direction[1])
 
 # Locations of obstacles, guard's initial position, map height/width
 IType = tuple[set[Coordinate],Coordinate,tuple[int,int]]
+
+def reach_check(height:int,width:int) -> Callable[[int,int],bool]:
+    return lambda x,y: x >=0 and x < width and y >= 0 and y < height
 
 def print_map(obstacles:set[Coordinate],tiles:set[Coordinate],size:tuple[int,int]):
     for y in range(size[1]):
@@ -39,19 +46,18 @@ def parse_input(input_content:str) -> IType:
 def star_one(data:IType) -> str:
     visited:set[Coordinate] = set()
     obstacles, guard, (width,height) = data
+    reach = reach_check(width,height)
     print(f"Field size {width} x {height}")
-    def in_field(x:int,y:int) -> bool:
-        return x >= 0 and x < width and y >= 0 and y < height
     direction = cycle(((0,-1),(1,0),(0,1),(-1,0)))
     
     while True:
         facing = next(direction)
         guard_n = guard
-        while guard_n not in obstacles and in_field(guard_n.x,guard_n.y):
+        while guard_n not in obstacles and reach(guard_n.x,guard_n.y):
             visited.add(guard_n)
             guard = guard_n
             guard_n = Coordinate(guard_n.x + facing[0],guard_n.y + facing[1])
-        if not in_field(guard_n.x,guard_n.y):
+        if not reach(guard_n.x,guard_n.y):
             break
     
     #print_map(obstacles,visited,(width,height))
@@ -59,7 +65,50 @@ def star_one(data:IType) -> str:
     return f"{len(visited)}"
 
 def star_two(data:IType) -> str:
-    pass
+    obstacles,guard,(width,height) = data
+    
+    directions = ((0,-1,0),(1,0,1),(0,1,2),(-1,0,3))
+    main_direction = cycle(directions)
+    heading = next(main_direction)
+    
+    check = reach_check(width,height)
+    
+    insert_positions:set[Coordinate] = set()
+    
+    while check(guard.x, guard.y):
+        step = guard.step(heading[0:2])
+        if step in obstacles:
+            heading = next(main_direction)
+        else:
+            #Treat the step as a ghost obstacle, and see if there is a collision that happens twice.
+            
+            inner_direction = cycle(directions)
+            inner_heading = next(inner_direction)
+            inner_guard = guard
+            for _ in range(heading[2]):
+                inner_heading = next(inner_direction)
+            
+            colisions:set[tuple[int,Coordinate]] = set()
+            while True:
+                inner_step = inner_guard.step(inner_heading[0:2])
+                if not check(inner_step.x, inner_step.y):
+                    break #The guard left the area; this is definitely not an infinite loop.
+                if inner_step in obstacles or inner_step == step:
+                    #The guard ran into something. Check if she did so before (same position, same heading.)
+                    event = (inner_heading[2],inner_guard)
+                    if event in colisions:
+                        insert_positions.add(step)
+                        print(colisions)
+                        print(event)
+                        break
+                    else:
+                        colisions.add(event)
+                        inner_heading = next(inner_direction)
+                else:
+                    inner_guard = inner_step
+            colisions.clear()
+            guard = step
+    return f"{len(insert_positions)}"
 
 if __name__ == "__main__":
     from pathlib import Path
