80f05d3313c56637b5f3e475868e7e5532c933b2
Vera van den Heuvel <ray10k@xmsnet.nl>
2024-12-16 09:30:30 +0100

    day 12 star 1

--- days/day_12.py
+++ days/day_12.py
@@ -1,21 +1,79 @@
-from dataclasses import dataclass;
+from dataclasses import dataclass
+from collections import namedtuple, deque
 
-@dataclass
-class InputItem:
-    """Representation of one 'unit' of input-data. May represent as little
-    as a single character from input, as much as the entire file, or anywhere
-    inbetween."""
-    a: str
+class GardenMap:
+    def __init__(self,plots:str,height:int,width:int):
+        self.plots = "".join(plots.split())
+        self.height = height
+        self.width = width
+        
+    def __getitem__(self,key) -> str:
+        x,y = key
+        if x < 0 or x >= self.width:
+            return ' '
+        if y < 0 or y >= self.height:
+            return ' '
+        offset = x + (y * self.width)
+        return self.plots[offset]
 
-IType = InputItem
+Point = namedtuple("Point","x y")
 
-def parse_input(input_content:str) -> list[IType]:
-    return list()
+@dataclass(init=False)
+class GardenPlot:
+    locations:frozenset[Point]
+    symbol:str
+    surface:int
+    circumference:int
+    
+    def __init__(self,symbol:str,locations:set[Point],circumference:int):
+        self.locations = frozenset(locations)
+        self.symbol = symbol[0]
+        self.surface = len(locations)
+        self.circumference = circumference
 
-def star_one(data:list[IType]) -> str:
-    pass
+IType = GardenMap
 
-def star_two(data:list[IType]) -> str:
+def parse_input(input_content:str) -> IType:
+    lines = input_content.splitlines()
+    w,h = len(lines[0]),len(lines)
+    return GardenMap(input_content,h,w)
+
+
+def star_one(data:IType) -> str:
+    visited:set[Point] = set()
+    plots:list[GardenPlot] = list()
+        
+    for y in range(data.height):
+        for x in range(data.width):
+            if Point(x,y) in visited:
+                continue
+            cur_sym:str = data[x,y]
+            fields:set[Point] = set()
+            circumference:int = 0
+            to_check:deque[Point] = deque()
+            to_check.append(Point(x,y))
+            print(f"Looking for {cur_sym}, starting from {Point(x,y)}")
+            while to_check:
+                here = to_check.popleft()
+                
+                if here in fields:
+                    continue
+                if data[here] != cur_sym:
+                    circumference += 1
+                    continue
+                
+                to_check.append(Point(here.x-1,here.y))
+                to_check.append(Point(here.x+1,here.y))
+                to_check.append(Point(here.x,here.y-1))
+                to_check.append(Point(here.x,here.y+1))
+                
+                visited.add(here)
+                fields.add(here)
+            plots.append(GardenPlot(cur_sym,fields,circumference))
+    retval = sum(p.circumference * p.surface for p in plots)
+    return f"{retval}"
+
+def star_two(data:IType) -> str:
     pass
 
 if __name__ == "__main__":
