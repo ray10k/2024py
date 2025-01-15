137e32affa16c94b23ca90241f4303123375580a
Vera vd Heuvel <ray10k@xmsnet.nl>
2025-01-15 16:00:24 +0100

    day 14 started work

--- days/day_14.py
+++ days/day_14.py
@@ -1,19 +1,72 @@
 from dataclasses import dataclass;
+import re
 
-@dataclass
-class InputItem:
-    """Representation of one 'unit' of input-data. May represent as little
-    as a single character from input, as much as the entire file, or anywhere
-    inbetween."""
-    a: str
+ROOM_WIDTH:int = 101
+ROOM_HEIGHT:int = 103
 
-IType = InputItem
+def wrap(val:int,limit:int) -> int:
+    if val > limit:
+        return val - limit
+    elif val < 0:
+        return limit - val
+    else:
+        return val
+
+class DynamicBot:
+    """A robot in 'mid-flight.'"""
+    def __init__(self, x, y, h, v):
+        self.x = x
+        self.y = y
+        self.h = h
+        self.v = v
+    
+    def update(self):
+        self.x = wrap(self.x + self.h,ROOM_WIDTH)
+        self.y = wrap(self.y + self.v,ROOM_HEIGHT)
+
+@dataclass(frozen=True,slots=True)
+class RobotStats:
+    """One security robot's initial state."""
+    start_x:int
+    start_y:int
+    velocity_x:int
+    velocity_y:int
+
+    def get_dynamic(self) -> DynamicBot:
+        return DynamicBot(self.start_x,self.start_y,self.velocity_x,self.velocity_y)
+
+IType = RobotStats
+BOT_PARSE = re.compile(r"p=(?P<x>\d+),(?P<y>\d+) v=(?P<h>\-?\d+),(?P<v>\-?\d+)")
 
 def parse_input(input_content:str) -> list[IType]:
-    return list()
+    retval:list[IType] = list()
+    for line in input_content.splitlines():
+        data = BOT_PARSE.match(line)
+        retval.append(RobotStats(int(data.group("x")),int(data.group("y")),int(data.group("h")),int(data.group("v"))))
+    return retval
 
 def star_one(data:list[IType]) -> str:
-    pass
+    bots = [init.get_dynamic() for init in data]
+    h_mid = int((ROOM_WIDTH - 1) / 2)
+    v_mid = int((ROOM_HEIGHT - 1) / 2)
+
+    for _ in range(100):
+        for bot in bots:
+            bot.update()
+    tl = tr = bl = br = 0
+
+    for bot in bots:
+        if bot.x < h_mid:
+            if bot.y < v_mid:
+                tl += 1
+            elif bot.y > v_mid:
+                bl += 1
+        elif bot.x > h_mid:
+            if bot.y < v_mid:
+                tr += 1
+            elif bot.y > v_mid:
+                br += 1
+    return f"{tl*tr*bl*br}"
 
 def star_two(data:list[IType]) -> str:
     pass
