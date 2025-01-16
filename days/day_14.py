bb1b76f38c6d698e914909257306bc7512aecb33
Vera vd Heuvel <ray10k@xmsnet.nl>
2025-01-16 15:33:43 +0100

    day 14 star 1

--- days/day_14.py
+++ days/day_14.py
@@ -5,24 +5,43 @@ ROOM_WIDTH:int = 101
 ROOM_HEIGHT:int = 103
 
 def wrap(val:int,limit:int) -> int:
-    if val > limit:
-        return val - limit
+    if val >= limit:
+        return val % limit
     elif val < 0:
-        return limit - val
+        return limit + val
     else:
         return val
 
+@dataclass
 class DynamicBot:
     """A robot in 'mid-flight.'"""
-    def __init__(self, x, y, h, v):
-        self.x = x
-        self.y = y
-        self.h = h
-        self.v = v
+    x:int
+    y:int
+    h:int
+    v:int
     
     def update(self):
         self.x = wrap(self.x + self.h,ROOM_WIDTH)
         self.y = wrap(self.y + self.v,ROOM_HEIGHT)
+
+def print_bot_map(bots:list[DynamicBot],w:int,h:int):
+    here:dict[tuple[int,int],int] = dict()
+    display = "0123456789abcdefghijklmnopqrstuvwxyz!"
+
+    for bot in bots:
+        count = here.get((bot.x,bot.y),0)
+        here[(bot.x,bot.y)] = count + 1
+
+    for y in range(h):
+        for x in range(w):
+            count = here.get((x,y),0)
+            if count == 0:
+                print(end=" ")
+            elif count < len(display):
+                print(display[count],end="")
+            else:
+                print("@",end="")
+        print()
 
 @dataclass(frozen=True,slots=True)
 class RobotStats:
@@ -71,7 +90,41 @@ def star_one(data:list[IType]) -> str:
 def star_two(data:list[IType]) -> str:
     pass
 
+def test_case():
+    global ROOM_HEIGHT,ROOM_WIDTH
+    ROOM_WIDTH = 11
+    ROOM_HEIGHT = 7
+    test_data = """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"""
+    parsed = parse_input(test_data)
+    a_bots = [b.get_dynamic() for b in parsed]
+
+    print_bot_map(a_bots,11,7)
+    print("+"*11)
+
+    for i in range(100):
+        for bot in a_bots:
+            bot.update()
+
+    print_bot_map(a_bots,11,7)
+
+        
+    ROOM_WIDTH = 101
+    ROOM_HEIGHT = 103
+
 if __name__ == "__main__":
+    test_case()
+    #print(test_case())
     from pathlib import Path
     source = input("Path to input data? (leave blank for 'input/day_14.txt')")
     if source == "" :
